99c2be874b47e4e2dee8b3389cff4a6b9d35bf0d
CrescenzoDeLuca <lmcdfa@gmail.com>
2019-01-21 12:06:34 +0100

    risolto

--- cerchio.c
+++ cerchio.c
@@ -9,11 +9,11 @@ double cir, supp;
 printf("si inserisca il raggio == ");
 scanf("%d", &rag);
 
-cir = (rag*2)*3.14;
+cir = (rag*2.0)*3.14;
 supp = (rag*rag)*3.14;
 
-printf("circonferenza == %lf\n", cir);
-printf("superficie == %lf\n", supp);
+printf("circonferenza == %f\n", cir);
+printf("superficie == %f\n", supp);
 
 return 0;
 }
